7ec1963eaac1b559a55e645fcb16162fb1876d9e
masa-futa <masakunn1215@gmail.com>
2022-12-07 08:47:44 +0900

    Swiftでもようブランチ作成

---
+++ swift_demo/demo/demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Masaki Futami on 2022/12/07.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
