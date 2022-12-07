cc503f2770fe0e84515132100b9a5a0898cc9184
masa-futa <masakunn1215@gmail.com>
2022-12-07 12:22:48 +0900

    flutterサンプル追加

---
+++ flutter_demo/lib/Basic.dart
@@ -0,0 +1,45 @@
+class Basic {
+  /// 非公開な名前付きコンストラクタ
+  Basic._();
+
+  /// ファクトリーメソッド
+  factory Basic.from({required String name}) {
+    print(name);
+    return Basic._();
+  }
+
+  /// 変数
+  var number = 10;
+  String name = 'Jon';
+
+  /// 定数
+  final age = 30;
+  static const message = 'Hello';
+
+  /// null safety
+  int? ages;
+
+  /// 関数（返り値あり + アロー構文）
+  int count() => number;
+
+  /// 関数（返り値なし）
+  void add() {
+    final person = Person(name, age);
+    print(person.name);
+    print(person.age);
+  }
+
+  /// 非同期関数
+  Future<String> future() async {
+    await Future.delayed(const Duration(milliseconds: 300));
+    return "";
+  }
+}
+
+/// クラス
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, this.age);
+}
