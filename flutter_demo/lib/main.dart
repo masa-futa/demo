b022ef6f66dc3f4ea698a9d431542bb5a8ab46a8
masa-futa <masakunn1215@gmail.com>
2022-12-07 13:49:41 +0900

    サンプル更新

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -12,8 +12,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: DemoPage()),
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: const Text('Demo'),
+          ),
+          body: const DemoPage()),
     );
   }
 }
@@ -24,14 +30,50 @@ class DemoPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ref.watch(futureProvider).when(
-        data: (data) => ProviderScope(
-            overrides: [provider.overrideWithValue('Hello')],
-            child: Center(
-              child: Text(
-                ref.watch(provider),
+          data: (data) => ListView.builder(
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: Card(
+                color: Colors.grey.shade800,
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          data[index].name,
+                          style: const TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        const SizedBox(height: 15),
+                        Text(
+                          data[index].text,
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 16),
+                        ),
+                        const SizedBox(height: 15),
+                        Row(children: [
+                          const Icon(
+                            Icons.star,
+                            size: 12,
+                            color: Colors.white,
+                          ),
+                          const SizedBox(width: 5),
+                          Text(
+                            '${data[index].count}',
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ])
+                      ]),
+                ),
               ),
-            )),
-        error: (error, status) => const SizedBox.shrink(),
-        loading: () => const CircularProgressIndicator());
+            ),
+            itemCount: data.length,
+          ),
+          error: (error, status) => const SizedBox.shrink(),
+          loading: () => const Center(child: CircularProgressIndicator()),
+        );
   }
 }
--- flutter_demo/lib/river_pod_demo.dart
+++ flutter_demo/lib/river_pod_demo.dart
@@ -1,15 +1,39 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// Provider
-final provider = Provider<String>((ref) => throw UnimplementedError());
+final provider = Provider<List<SampleModel>>((ref) => []);
 
 /// StateProvider
 final stateProvider = StateProvider<int>((ref) => 0);
 
 /// FutureProvider
-final futureProvider = FutureProvider.autoDispose<String>((ref) async {
-  await Future.delayed(const Duration(milliseconds: 300));
-  return "hello";
+final futureProvider = FutureProvider<List<SampleModel>>((ref) async {
+  await Future.delayed(const Duration(seconds: 3));
+  return const [
+    SampleModel(
+        name: 'open-source-ios-apps',
+        text: ':iphone: Collaborative List of Open-Source iOS Apps',
+        count: 32602),
+    SampleModel(
+        name: 'CodeEdit',
+        text:
+            'CodeEdit App for macOS-Elevate your code editing experience. Open Source, free forever.',
+        count: 15558),
+    SampleModel(
+        name: 'eul',
+        text: 'macOS status monitoring app written in SwiftUI.',
+        count: 8161),
+    SampleModel(
+        name: 'swift-composable-architecture',
+        text:
+            'xxxxxxxxxxxxx.xxxxxxxxxxxxx.xxxxxxxxxxxxx.xxxxxxxxxxxx.xxxxxxxxx.',
+        count: 5012),
+    SampleModel(
+        name: 'sampel.sampel.sampel',
+        text:
+            'xxxxxxxxxxxxx.xxxxxxxxxxxxx.xxxxxxxxxxxxx.xxxxxxxxxxxx.xxxxxxxxx.',
+        count: 4401),
+  ];
 });
 
 /// StateNotifierProvider
@@ -34,4 +58,16 @@ class Demo {
 
   final String name;
   final int age;
+}
+
+class SampleModel {
+  const SampleModel({
+    required this.name,
+    required this.text,
+    required this.count,
+  });
+
+  final String name;
+  final String text;
+  final int count;
 }
